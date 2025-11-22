0381eef322f5039bc3b08f752b8c2d485dd29bbf
mr-khristom <sebastian.khristom@gmail.com>
2025-11-23 01:47:11 +0800

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Important for GitHub Pages to find assets
+  base: '/Project_Jar-Of-Us/',
 })
